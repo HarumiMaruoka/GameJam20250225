dcbc5016011c193717c8bfe06c1f00493827bd22
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-02-25 14:25:15 +0900

    [add] Soundの追加

--- Assets/Scripts/Foundation/SceneChanger.cs
+++ Assets/Scripts/Foundation/SceneChanger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,8 +7,15 @@ using UnityEngine.SceneManagement;
 /// <summary>シーン切り替え</summary>
 public static class SceneChanger
 {
-    public static async UniTask ChangeScene(string sceneName, Func<UniTask> callback = null)
+    public static async UniTask ChangeScene(string sceneName,CancellationTokenSource cts, Func<UniTask> callback = null)
     {
+
+        if (cts.IsCancellationRequested)
+        {
+            Debug.LogWarning("シーン切り替えがキャンセルされました");
+            return;
+        }
+        
         if (callback != null)
         {
             await callback.Invoke();
