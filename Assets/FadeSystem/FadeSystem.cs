1a66c5b214608d6668fa075e2d8f49b6e6532bae
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-02-26 13:31:47 +0900

    [update] Fadeの設定を編集

--- Assets/FadeSystem/FadeSystem.cs
+++ Assets/FadeSystem/FadeSystem.cs
@@ -1,46 +1,16 @@
 ﻿using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Windows;
-using Cysharp.Threading;
-
 public class FadeSystem : MonoBehaviour
 {
-    //[SerializeField, Tooltip("ここにフェードインorフェードアウトするまでの時間を設定する")] float toFadeTime = 1;
     [SerializeField, Tooltip("チェックが入っていればフェードイン、入ってなければフェードアウト")] bool fadeIn = true;
     [SerializeField, Tooltip("DelayTime")]private int _fadeDelayTime = 10;
     FadeImage fadeImage;
 
     private const int _fadeLoop = 100;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         fadeImage = GetComponent<FadeImage>();
-    }
-
-    private void Update()
-    {
-        /* Debug.Log("読み込みは終了済み");
-        if (UnityEngine.Input.GetKeyDown(KeyCode.Return))
-        {
-            FadeOut();
-        }
-        if (UnityEngine.Input.GetKeyDown(KeyCode.V))
-        {
-            FadeIn();
-        }*/
-
-        if (fadeImage.Range < 0)
-        {
-            fadeImage.Range = 0;
-        }
-        else if (fadeImage.Range > 1)
-        {
-            fadeImage.Range = 1;
-        }
     }
 
     public async UniTask FadeIn()
@@ -55,7 +25,7 @@ public class FadeSystem : MonoBehaviour
         await FadeIn_Out();
     }
 
-    public async UniTask FadeIn_Out()
+    private async UniTask FadeIn_Out()
     {
         for (int i = 0; i < _fadeLoop; i++)
         {
--- Assets/Scripts/Title/KetteiButton.cs
+++ Assets/Scripts/Title/KetteiButton.cs
@@ -1,5 +1,6 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class KetteiButton : MonoBehaviour
@@ -7,22 +8,25 @@ public class KetteiButton : MonoBehaviour
     [SerializeField] private Button _button;
     [SerializeField] private string _sceneName;
     [SerializeField] private EnterUserName _enterUserName;
+    [SerializeField] private FadeSystem _fadeSystem;
+    private CancellationTokenSource _cts;
 
     void Start()
     {
-        _button.onClick.AddListener(OnClick);
+        _cts = new CancellationTokenSource();
+        _button.onClick.AddListener(async () =>await OnClick());
     }
 
-    void OnClick()
+    public async UniTask OnClick()
     {
         if (_enterUserName.userName.Length > 0)
         {
-            SceneManager.LoadScene(_sceneName);
+            await SceneChanger.ChangeScene(_sceneName,_cts,async () => await  _fadeSystem.FadeOut());
         }
         else
         {
             _enterUserName.userName = "Player";
-            SceneManager.LoadScene(_sceneName);
+            await SceneChanger.ChangeScene(_sceneName,_cts,async () => await  _fadeSystem.FadeOut());
         }
     }
 }
