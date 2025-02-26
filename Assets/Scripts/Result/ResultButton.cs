cf0dc88eb15e9e5b3f265c5faa6271c51aa4bee2
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2025-02-26 17:20:21 +0900

    [update]ResultSceneの編集

--- Assets/Scripts/Result/ResultButton.cs
+++ Assets/Scripts/Result/ResultButton.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 public class ResultButton : MonoBehaviour
 {
     [SerializeField] Button _resultButton;
     [SerializeField] string _sceneName;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] FadeSystem _fadeSystem;
+    private CancellationTokenSource _cts;
+
     void Start()
     {
+        _cts = new CancellationTokenSource();
         _resultButton.onClick.AddListener(SceneChange);
     }
 
-    void SceneChange()
+    async void SceneChange()
     {
-        SceneManager.LoadScene(_sceneName);
+        _audioSource.Play();
+        await SceneChanger.ChangeScene(_sceneName, _cts, async () => await _fadeSystem.FadeOut());
     }
     
 }
